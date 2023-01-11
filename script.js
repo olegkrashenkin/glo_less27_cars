00022d31095fc975517e73d10588fdb1b94d58a6
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2023-01-11 12:41:56 +0300

    comm

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const select = document.getElementById('select')
+const text = document.getElementById('text')
+
+const getData = fetch('./cars.json')
+    .then(res => res.json())
+    .catch(err => console.log(err.message))
+
+
+const addOptions = () => {
+    getData.then(data => {
+        data.cars.forEach(el => {
+            const newOption = document.createElement('option')
+
+            newOption.value = el['brand']
+            newOption.textContent = el['brand']
+
+            select.append(newOption)
+        });
+    })
+}
+
+const showCarInfo = (brand) => {
+    text.innerHTML === '' ? select.options[0].disabled = true : text.innerHTML = ''
+
+    getData.then(data => {
+        data.cars.forEach(el => {
+            if (brand === el['brand']) {
+                const newText = document.createElement('p')
+
+                newText.innerHTML = `Car: ${el['brand']} ${el['model']}<br>Price: ${el['price']}$`
+
+                text.append(newText)
+            }
+        });
+    })
+}
+
+select.addEventListener('change', (e) => {
+    showCarInfo(e.target.value)
+})
+
+addOptions()
